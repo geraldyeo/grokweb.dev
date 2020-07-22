e5dfa1a7223dd44907537801faebd23d8be8c2ef
Gerald Yeo <gerald.yeo@gmail.com>
2020-07-22 10:36:56 +0800

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: ['react-app', 'eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint'],
+  plugins: ['@typescript-eslint'],
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+# grokweb.dev monorepo
